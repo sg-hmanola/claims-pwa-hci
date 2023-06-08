f7462fce846eebf33b2e865d1071e85b812c9cbb
kaushal <kaushal8650@gmail.com>
2023-06-08 18:04:06 +0530

    code fix logback error

--- src/main/java/claimspwahci/serviceImpl/ClaimAdjudicationRuleServiceImpl.java
+++ src/main/java/claimspwahci/serviceImpl/ClaimAdjudicationRuleServiceImpl.java
@@ -2,6 +2,7 @@ package claimspwahci.serviceImpl;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,6 @@ import claimspwahci.bean.HCIClient;
 import claimspwahci.bean.Response;
 import claimspwahci.generated.Dspartprice;
 import claimspwahci.service.ClaimAdjudicationRuleService;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
@@ -22,16 +22,13 @@ public class ClaimAdjudicationRuleServiceImpl implements ClaimAdjudicationRuleSe
 
 	@Override
 	public Response getHciPartPrice(List<String> partNumber, String dealerNumber, String roDate, HttpHeaders headers) {
-		log.info("Inside getHciPartPrice method under service Impl");
-		System.out.println("getHciPartPrice method under service");
 		Dspartprice partPriceresponse = null;
 		try {
 			partPriceresponse = hCIClient.getPartPrice(partNumber, dealerNumber, roDate);
-			System.out.println("Response :: " + partPriceresponse);
+			log.info("Response :: {}",partPriceresponse);
 		} catch (Exception e) {
-			log.error("Exception occured " + e.getMessage());
-			System.out.println("Exception occured " + e.getMessage());
 			e.printStackTrace();
+			log.error("Exception occured " + e.getMessage());
 		}
 		return new Response(ApplicationConstants.IS_SUCCESS, "CMS", ApplicationConstants.FAILED,
 				ApplicationConstants.INTERNAL_ERROR_STATUS_CODE, ApplicationConstants.INTERNAL_ERROR_STATUS_MESSAGE,
