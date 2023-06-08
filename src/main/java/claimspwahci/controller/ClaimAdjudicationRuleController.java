797d818ef33e74862a256af406b0cd425a96aa53
kaushal <kaushal8650@gmail.com>
2023-06-08 12:57:34 +0530

    code fix logback error

--- src/main/java/claimspwahci/controller/ClaimAdjudicationRuleController.java
+++ src/main/java/claimspwahci/controller/ClaimAdjudicationRuleController.java
@@ -1,20 +1,21 @@
 package claimspwahci.controller;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import claimspwahci.bean.Response;
 import claimspwahci.service.ClaimAdjudicationRuleService;
 
 @RestController
+@Slf4j
 public class ClaimAdjudicationRuleController {
 	@Autowired
 	private ClaimAdjudicationRuleService claimAdjudicationRuleService;
@@ -26,8 +27,14 @@ public class ClaimAdjudicationRuleController {
 			@RequestParam(value = "partNumber") List<String> partNumber,
 			@RequestParam(value = "dealerNumber") String dealerNumber, @RequestParam(value = "roDate") String roDate,
 			@RequestHeader HttpHeaders headers) {
-
+		log.info("request received!!");
 		return new ResponseEntity<>(
 				claimAdjudicationRuleService.getHciPartPrice(partNumber, dealerNumber, roDate, headers), HttpStatus.OK);
+	}
+	@GetMapping("/tests")
+	public String test(){
+		System.out.println("hello");
+		log.info("Hello world!!");
+		return "Hello test application!!";
 	}
 }
